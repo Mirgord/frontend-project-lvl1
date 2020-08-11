def88f1f58c696789454d917eb8cac6e04dbc7af
Vladimir <uptonpark82@gmail.com>
2020-08-11 20:43:15 +0300

    correction of comments in the 4th iteration

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -3,7 +3,7 @@ import getRandomInRange from '../utils.js';
 
 const discription = ('Answer "yes" if the number is even, otherwise answer "no"');
 
-const isEven = (num) => ((num) % 2 === 0 ? ('yes') : 'no');
+const isEven = (num) => ((num % 2 === 0) ? 'yes' : 'no');
 
 const sayEvenOrNot = () => {
   const rounds = 3;
